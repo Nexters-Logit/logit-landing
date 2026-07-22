7dd576926558ba0496fe6fd25aa12b25c061f2f9
LimJaeHyeon9298 <limjh9298@gmail.com>
2026-07-22 10:29:15 +0900

    feat: 다크 섹션 CTA 텍스트 및 버튼 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,6 +52,55 @@ export default function Home() {
             height: "451px",
           }}
         />
+        {/* CTA 박스 */}
+        <div
+          style={{
+            position: "absolute",
+            top: "511px",
+            left: "527px",
+            width: "866px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "32px",
+          }}
+        >
+          <p
+            style={{
+              color: "#FFF",
+              textAlign: "center",
+              fontFamily: "Pretendard",
+              fontSize: "40px",
+              fontWeight: 700,
+              lineHeight: "120%",
+              whiteSpace: "nowrap",
+            }}
+          >
+            경험이 자산이 되는 자소서 작성, 지금 바로 시작해보세요
+          </p>
+          <button
+            style={{
+              display: "flex",
+              width: "586px",
+              height: "66px",
+              padding: "14px 24px",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "13.702px",
+              borderRadius: "14px",
+              background: "#40A5FF",
+              border: "none",
+              cursor: "pointer",
+              color: "#FFF",
+              fontFamily: "Pretendard",
+              fontSize: "18px",
+              fontWeight: 600,
+              lineHeight: "140%",
+            }}
+          >
+            로짓 무료로 시작하기
+          </button>
+        </div>
       </div>
     </main>
   );
